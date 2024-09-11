4e1a96637cac864e641faa8b51e6bfda5e0a3164
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-09-11 22:01:52 +0530

    Fifth Task

---
+++ Task 5/Solution.sql
@@ -0,0 +1,14 @@
+SELECT DATE_FORMAT(CURRENT_TIME(), '%H:%i:%s') AS formatted_time;
+
+SELECT DATE_FORMAT(CURRENT_DATE(), '%Y-%m-%d') AS formatted_Date;
+
+SELECT DayName(CURRENT_DATE);
+
+
+/* We use char to represent Fixed length strings  in MySQL*/
+
+/*We can use decimal or double to store 123.456*/
+
+
+
+
